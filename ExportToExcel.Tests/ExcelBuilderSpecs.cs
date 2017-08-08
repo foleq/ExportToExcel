55726055dda553b8bdd5d7b5fc0ae98dc03ea79a
foleq <piotrekgda90@gmail.com>
2017-08-08 11:16:31 +0200

    Implement building empty excel file

---
+++ ExportToExcel.Tests/ExcelBuilderSpecs.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using developwithpassion.specifications.rhinomocks;
+using DocumentFormat.OpenXml.Packaging;
+using Machine.Specifications;
+
+namespace ExportToExcel.Tests
+{
+    public class CleanExcelBuilderResult : ICleanupAfterEveryContextInAssembly
+    {
+        public void AfterContextCleanup()
+        {
+            ExcelBuilderSpecs.Result.Close();
+        }
+    }
+
+    [Subject(typeof(ExcelBuilder))]
+    internal abstract class ExcelBuilderSpecs : Observes<ExcelBuilder>
+    {
+        Because of = () =>
+        {
+            var excelBytes = sut.GetExcelBytes();
+            sut.Dispose();
+            Result = GetDocumentFrom(excelBytes);
+        };
+
+        private static SpreadsheetDocument GetDocumentFrom(byte[] bytes)
+        {
+            using (var stream = new MemoryStream(bytes))
+            {
+                return SpreadsheetDocument.Open(stream, false);
+            }
+        }
+
+        public static SpreadsheetDocument Result;
+    }
+
+    internal class When_creating_excel : ExcelBuilderSpecs
+    {
+        It should_build_correctly_formatted_excel = () =>
+        {
+            Result.ShouldNotBeNull();
+        };
+    }
+}
---
+++ ExportToExcel/ExcelBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace ExportToExcel
+{
+    public interface IExcelBuilder
+    {
+        byte[] GetExcelBytes();
+    }
+
+    public class ExcelBuilder : IExcelBuilder, IDisposable
+    {
+        private readonly MemoryStream _memoryStream;
+        private readonly SpreadsheetDocument _document;
+
+        public ExcelBuilder()
+        {
+            _memoryStream = new MemoryStream();
+            _document = SpreadsheetDocument.Create(_memoryStream, SpreadsheetDocumentType.Workbook);
+            _document.AddWorkbookPart();
+        }
+
+        public byte[] GetExcelBytes()
+        {
+            _document.Close();
+            return _memoryStream.ToArray();
+        }
+
+        public void Dispose()
+        {
+            _document?.Dispose();
+            _memoryStream?.Dispose();
+        }
+    }
+}
